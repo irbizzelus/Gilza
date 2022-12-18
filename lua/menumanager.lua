3a67ccbe96e32b68c093e44bf4bde19d9d8bd835
irbizzelus <117110119+irbizzelus@users.noreply.github.com>
2022-12-18 22:17:57 +0300

    1.5.3 final

--- lua/menumanager.lua
+++ lua/menumanager.lua
@@ -4,19 +4,20 @@ end
 
 function Gilza:changelog_message()
 	DelayedCalls:Add("Gilza_showchangelogmsg_delayed", 1, function()
-		if not Gilza.customguns.version or Gilza.customguns.version < 1.52 then
+		if not Gilza.customguns.version or Gilza.customguns.version < 1.53 then
 			local menu_options = {}
 			menu_options[#menu_options+1] ={text = "Check full changelog", data = nil, callback = Gilza_linkchangelog}
 			menu_options[#menu_options+1] = {text = "Cancel", is_cancel_button = true}
-			local message = "1.5.2 update changelog:\n- Fixed broomstick ammo pick while using the AP kit\n- Fixed custom weapons not saving properly\n- Rebalances to all AR's, SMG's and Pistols. More info in the changelog."
+			local message = "1.5.3 update changelog:\n- 232 update support added, including snowballs, sticky grenades and the secondary sniepr. Sniper was tweaked a bit compraed to vanilla, more info in the changelog\n- Buffs to flamethrowers, normal grenades, basic (from any perk deck) grenade pick ups\n- Nerfs to the brawler perk deck\n- Updated health for some units that were missing from overall health increase"
 			local menu = QuickMenu:new("Gilza", message, menu_options)
 			menu:Show()
-			-- reset anyone on version 1.5, if they are unforunate enough to have it
+			
+			-- reset anyone on version 1.5, if they were unforunate enough to have it
 			if Gilza.customguns.version and Gilza.customguns.version == 1.5 then
 				managers.skilltree:reset_specializations()
 			end
 			
-			Gilza.customguns.version = 1.52
+			Gilza.customguns.version = 1.53
 			Gilza.Save_gunz()
 		end
 	end)
